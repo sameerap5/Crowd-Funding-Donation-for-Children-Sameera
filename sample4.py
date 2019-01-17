163f24cfceca640a151de9008204c13944697bf1
vivek14632 <vksingh.iiitb@gmail.com>
2019-01-17 15:10:58 -0500

    Update sample4.py

--- sample4.py
+++ sample4.py
@@ -5,20 +5,30 @@ import time
 from selenium.webdriver.chrome.options import Options
 import openpyxl
 import csv
+import time
 
 chromeoptions = Options()
 driver = webdriver.Chrome(executable_path="D:\\USF\Selenium\chromedriver.exe", chrome_options=chromeoptions)   #file location and browser which we will use to open
-driver.get("https://www.donorschoose.org/donors/search.html")
+base_url="https://www.donorschoose.org/donors/search.html?page="
+page_counter=1
+
+while(True):
+    try:
+        driver.get(base_url+str(page_counter))
+        time.sleep(10)
+
+        
+        list = []
+        for a in driver.find_elements_by_xpath('.//a'):
+            # print(a.get_attribute('href'))
+            list.append(a.get_attribute('href'))
+            #b=pd.DataFrame(b)
+
+            #list.to_csv(csvFile,sep=',')
+    except:
+        break
 
 csvFile = "D:\\USF\Selenium\sample4.csv"
-list = []
-for a in driver.find_elements_by_xpath('.//a'):
-   # print(a.get_attribute('href'))
-    list.append(a.get_attribute('href'))
-    #b=pd.DataFrame(b)
-
-#list.to_csv(csvFile,sep=',')
-
 with open(csvFile, "w") as output:
     writer = csv.writer(output, lineterminator='\n')
     for val in list:
