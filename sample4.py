0424de18d28947a765b9399da6191c23199b8905
vivek14632 <vksingh.iiitb@gmail.com>
2019-01-17 15:26:58 -0500

    Update sample4.py

--- sample4.py
+++ sample4.py
@@ -8,18 +8,17 @@ import csv
 import time
 
 chromeoptions = Options()
-driver = webdriver.Chrome(executable_path="D:\\USF\Selenium\chromedriver.exe", chrome_options=chromeoptions)   #file location and browser which we will use to open
+driver = webdriver.Chrome(executable_path="C:\\Users\\vivek4\\Downloads\\chromedriver\\chromedriver.exe", chrome_options=chromeoptions)   #file location and browser which we will use to open
 base_url="https://www.donorschoose.org/donors/search.html?page="
 page_counter=1
 
+list = []
 while(True):
     try:
         driver.get(base_url+str(page_counter))
         time.sleep(10)
-
-        
-        list = []
-        for a in driver.find_elements_by_xpath('.//a'):
+        all_urls = driver.find_elements_by_xpath("//div[@class='search-results']")
+        for a in all_urls[0]:
             # print(a.get_attribute('href'))
             list.append(a.get_attribute('href'))
             #b=pd.DataFrame(b)
@@ -28,7 +27,7 @@ while(True):
     except:
         break
 
-csvFile = "D:\\USF\Selenium\sample4.csv"
+csvFile = "sample4.csv"
 with open(csvFile, "w") as output:
     writer = csv.writer(output, lineterminator='\n')
     for val in list:
