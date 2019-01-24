09d01d3b2ceebf8dbea03544ab8af13716725a8b
vivek14632 <vksingh.iiitb@gmail.com>
2019-01-24 17:14:22 -0500

    Update sample4.py

--- sample4.py
+++ sample4.py
@@ -12,24 +12,33 @@ driver = webdriver.Chrome(executable_path="C:\\Users\\vivek4\\Downloads\\chromed
 base_url="https://www.donorschoose.org/donors/search.html?page="
 page_counter=1
 
-list = []
+list_m = []
 while(True):
     try:
         driver.get(base_url+str(page_counter))
         time.sleep(10)
-        all_urls = driver.find_elements_by_xpath("//div[@class='search-results']")
-        for a in all_urls[0]:
+        #all_urls = driver.find_elements_by_xpath("//div[@class='search-results']")
+        #div_m=driver.find_elements_by_xpath("//div[@data-reactid='218']")
+        all_urls1 = driver.find_elements_by_xpath("//a[@class='project-card']")
+        all_urls2 = driver.find_elements_by_xpath("//a[@class='project-card match-dyi with-match']")
+        for a in all_urls1:
             # print(a.get_attribute('href'))
-            list.append(a.get_attribute('href'))
+            list_m.append(a.get_attribute('href'))
+            #b=pd.DataFrame(b)
+        for a in all_urls2:
+            # print(a.get_attribute('href'))
+            list_m.append(a.get_attribute('href'))
             #b=pd.DataFrame(b)
 
             #list.to_csv(csvFile,sep=',')
         page_counter+=1
+        if (page_counter>1):
+            break
     except:
         break
 
 csvFile = "sample4.csv"
 with open(csvFile, "w") as output:
     writer = csv.writer(output, lineterminator='\n')
-    for val in list:
+    for val in list_m:
         writer.writerow([val])
