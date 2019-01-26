60c1980b893475a32df775cb16a4e885ba6bb698
sameerap5 <46380219+sameerap5@users.noreply.github.com>
2019-01-26 12:48:51 -0500

    Update download_pages.py

--- download_pages.py
+++ download_pages.py
@@ -27,7 +27,36 @@ for url in urls:
 		header_list=['Donors','Donors still needed', 'goal','title']
 		data_m=data_m.reindex(columns=header_list)
 		data_m = data_m.append({'Donors':tmp[0], 'Donors still needed':tmp[1],'goal':tmp[2],'title':tmp[3]}, ignore_index=True)
+		
+		
+		#*******************sam additional codes
+		#expiry=driver.find_element_by_xpath("//p[@class='undefined-sticky-wrapper']")
+		#tmp.append(expiry[0].text)
+
+		#teacher_img=driver.find_element_by_xpath("//a[@class='classroom-photo js-classroom-photo-format-retina-bg']")
+		#tmp.append(teacher_img[0].text)
+
+		#don_goes=driver.find_element_by_xpath("//h2 [@class='desktop js-materials']")
+		#tmp.append(don_goes[0].text)
+
+		#don_goes = driver.find_element_by_xpath("//td [@class='itemName']")
+		#tmp.append(don_goes[0].text)    						#web element does not support indexing
+
+
+
+		#pro_act=driver.find_element_by_xpath("//h2 [@class='desktop ']")
+		#tmp.append(pro_act[0].text)
+
+
+#****************sam addition ends
+
+
+		header_list=['Donors','Donors still needed', 'goal','title','expiry','donation_goes','teacher_img','don_goes','pro_act']
+		data_m=data_m.reindex(columns=header_list)
+		data_m = data_m.append({'Donors':tmp[0], 'Donors still needed':tmp[1],'goal':tmp[2],'title':tmp[3], 'expiry':tmp[4],'teacher_img':tmp[5],'don_goes':tmp[6] , 'pro_act':tmp[7]}, ignore_index=True)   #avalues getting added here
 	except:
+		
+
 		print(url)
 data_m.to_csv('data.csv')
 	
