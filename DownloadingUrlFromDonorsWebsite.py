dac05996df0f92e35f7d15d21c1c5f6a49313238
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2019-06-01 16:01:08 -0400

    Update DownloadingUrlFromDonorsWebsite.py

--- DownloadingUrlFromDonorsWebsite.py
+++ DownloadingUrlFromDonorsWebsite.py
@@ -1,5 +1,5 @@
-#Download url from the website "https://www.donorschoose.org"
-#argv[1]="D:\\USF\Selenium\chromedriver.exe"
+# Download url from the website "https://www.donorschoose.org"
+# argv[1]="D:\\USF\Selenium\chromedriver.exe"
 # The outcome.....
 
 import sys
