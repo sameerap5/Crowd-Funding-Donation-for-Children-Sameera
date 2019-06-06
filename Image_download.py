0270a70a3ad275610a5d742eaf8cba87ddd08d2a
Sameera Prasad <46380219+sameerap5@users.noreply.github.com>
2019-06-06 17:38:05 -0400

    Update Image_download.py

--- Image_download.py
+++ Image_download.py
@@ -1,3 +1,12 @@
+
+#argv[1]="D:\\USF\Selenium\chromedriver.exe"
+#argv[2]='D:\\USF\\Selenium\\'
+# The outcome will be saved with the following name For eg. "30#####. png" in the same location where the file has been executed
+desti
+# The example running script will look like the following:
+# AuthorName: Sameera Prasad, Vivek Singh
+
+#################################################################################################
 # download pages
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,8 +21,7 @@ import datetime
 import shutil
 import os 
 import sys
-#argv[1]="D:\\USF\Selenium\chromedriver.exe"
-#argv[2]='D:\\USF\\Selenium\\'
+
 
 
 chromeoptions = Options()
@@ -26,12 +34,12 @@ except:
 	pass
 #time.sleep(10)
 
-fp=open('20190213.csv')
+fp=open('20190213.csv')				#file containin g all the url generated
 urls=fp.readlines()
 data_m=pd.DataFrame()
 now = datetime.datetime.now()
 current_date = now.strftime("%Y-%m-%d")			#prints today's date and stores in current_date variable
-image_path=argv[2]				#path of the folder
+image_path=sys.argv[2]				#path of the folder
 destination=os.path.join(image_path+current_date)			#file being name with today's date
 os.makedirs(destination)				#folder getting created with name destionation hence can be ignored
 for url in urls:
